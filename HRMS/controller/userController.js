7e6a8c1ab712cc2b0de38e13f043064a4b16cca7
jasper <jas131059@gmail.com>
2023-01-12 19:45:36 +0530

    bug fixed

--- HRMS/controller/userController.js
+++ HRMS/controller/userController.js
@@ -35,10 +35,10 @@ export const reg=async (req,res)=>{
     }    
 }
 
-// Update the User
+//-------- Update the User
 export const updateUser= async (req, res) => {
     try {
-        const user = await User.findByIdAndUpdate(req.body.id, {$set: req.body})
+        const user = await User.findByIdAndUpdate(req.                                                                                      body.id, {$set: req.body})
         res.status(200).json({meesage:"update success"})
     } catch (error) {
         res.status(400).json({message:error.message});
