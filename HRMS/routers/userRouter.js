9afe24fc8517c016bba0266a5fffedcbb83259a4
jasper <jas131059@gmail.com>
2023-01-13 15:24:00 +0530

    bug fixed

--- HRMS/routers/userRouter.js
+++ HRMS/routers/userRouter.js
@@ -1,26 +1,30 @@
 import express from "express";
-import { login, reg, getAll, ownerReg, deleteUser, profile, updateUser, UserFam, updateFam, getFam, deleteFam, createAddress, updateAddress, viewUserAddress, currentCompany, currentCompanyView, currentCompanyUpdate, addDocument, viewDocument, updateDocument, addPreviousCompany, viewPreviousCompany, previousCompanyUpdate } from "../controller/userController.js";
+import { login, reg, getAll, ownerReg, deleteUser, profile, updateUser, UserFam, updateFam, getFam, deleteFam, createAddress, updateAddress, viewUserAddress, currentCompany,
+     currentCompanyView, currentCompanyUpdate, addDocument, viewDocument, updateDocument, addPreviousCompany, viewPreviousCompany, previousCompanyUpdate } from "../controller/userController.js";
 
 import auth from "../middleware/auth.js";
 import authz from "../middleware/authz.js";
 
 const router = express.Router();
 
+//V1
 router.post("/login",login)
 router.get("/profile",auth,profile)
 router.post("/v1/ownerReg",ownerReg)
-// router.delete("/v2/currentCompany",[auth,authOwner],currentCompany)
 
+// Family Details------------
 router.post("/v2/userfamily", [auth, authz], UserFam)
 router.post("/v2/updatefamily", [auth, authz], updateFam)
 router.get("/v2/getbyid", [auth, authz], getFam)
 router.delete("/v2/deletefam", [auth, authz], deleteFam)
 
+// User Details------------
 router.post("/v2/reg", [auth, authz], reg)
 router.put("/v2/update", [auth, authz], updateUser)
 router.delete("/v2/deleteUser", [auth, authz], deleteUser)
 router.get("/get", [auth, authz], getAll)
 
+// Address Details------------
 router.post("/v2/Useraddress", [auth, authz], createAddress)
 router.put("/v2/Updateaddress", [auth, authz], updateAddress)
 router.get("/v2/getaddress", [auth, authz], viewUserAddress)
