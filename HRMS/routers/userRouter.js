19ce46f613ec0ff685eefa7208465bc0a1e12e0e
Masaniprakash <masaniprakash05@gmail.com>
2023-01-13 10:49:39 +0530

    bug fixed

--- HRMS/routers/userRouter.js
+++ HRMS/routers/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, reg ,getAll, ownerReg,deleteUser} from "../controller/userController.js";
+import { login, reg ,getAll, ownerReg,deleteUser, updateUser} from "../controller/userController.js";
 import auth from "../middleware/auth.js";
 import authz from "../middleware/authz.js";
 
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post("/login",login)
 router.post("/v1/ownerReg",ownerReg)
 router.post("/v2/reg",[auth,authz],reg)
-router.put("/v2/update", [auth, authz], reg)
+router.put("/v2/update", [auth, authz], updateUser)
 router.delete("/v2/deleteUser",[auth,authz],deleteUser)
 router.get("/get",[auth,authz],getAll)  
 
